d9bab798d47faf600a3f818e878f08082db442b8
meruyert.bekzhigit <meruyert.bekzhigit@gmail.com>
2022-08-01 13:38:14 +0200

    Search bar added + some refactoring

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -29,79 +29,110 @@ class HomePageState extends State<HomePage> {
               const EdgeInsets.only(left: 16, top: 24, right: 16, bottom: 24),
           child: Column(
             children: [
-              Row(
-                children: [
-                  const IconButton(
-                    icon:
-                        Icon(Icons.add_a_photo, color: Colors.black, size: 30),
-                    onPressed: null,
-                  ),
-                  Expanded(
-                    child: Container(),
-                  ),
-                  const IconButton(
-                    icon: Icon(Icons.bar_chart_rounded,
-                        color: Colors.black, size: 30),
-                    onPressed: null,
-                  ),
-                  const SizedBox(width: 15),
-                  const IconButton(
-                    icon: Icon(Icons.star_rate_rounded,
-                        color: Colors.black, size: 30),
-                    onPressed: null,
-                  ),
-                  const SizedBox(width: 15),
-                  const IconButton(
-                    icon: Icon(Icons.notifications,
-                        color: Colors.black, size: 30),
-                    onPressed: null,
-                  ),
-                ],
-              ),
-              Row(),
+              buildQuickAccessMenuBar(),
               const SizedBox(height: 16),
-              Row(
-                children: [
-                  Expanded(
-                    child: CupertinoSlidingSegmentedControl<
-                        AvailableProductSegments>(
-                      backgroundColor: Colors.transparent,
-                      groupValue: _selectedSegment,
-                      onValueChanged: (AvailableProductSegments? value) {
-                        if (value != null) {
-                          setState(() {
-                            _selectedSegment = value;
-                          });
-                        }
-                      },
-                      children: const <AvailableProductSegments, Widget>{
-                        AvailableProductSegments.accounts: Text(
-                          'Accounts',
-                          style: TextStyle(color: Colors.black, fontSize: 15),
-                        ),
-                        AvailableProductSegments.cards: Text(
-                          'Cards',
-                          style: TextStyle(color: Colors.black, fontSize: 15),
-                        ),
-                        AvailableProductSegments.stocks: Text(
-                          'Stocks',
-                          style: TextStyle(color: Colors.black, fontSize: 15),
-                        ),
-                        AvailableProductSegments.crypto: Text(
-                          'Crypto',
-                          style: TextStyle(color: Colors.black, fontSize: 15),
-                        ),
-                        AvailableProductSegments.vaults: Text(
-                          'Vaults',
-                          style: TextStyle(color: Colors.black, fontSize: 15),
-                        ),
-                      },
-                    ),
-                  ),
-                ],
-              ),
+              buildInputSearchBar(context),
+              const SizedBox(height: 16),
+              buildAvailableProductSegments(context)
             ],
           )),
+    );
+  }
+
+  Widget buildQuickAccessMenuBar() {
+    return Row(
+      children: [
+        const IconButton(
+          icon: Icon(Icons.add_a_photo, color: Colors.black, size: 35),
+          onPressed: null,
+        ),
+        Expanded(
+          child: Container(),
+        ),
+        const IconButton(
+          icon: Icon(Icons.bar_chart_rounded, color: Colors.black, size: 35),
+          onPressed: null,
+        ),
+        const SizedBox(width: 15),
+        const IconButton(
+          icon: Icon(Icons.star_rate_rounded, color: Colors.black, size: 35),
+          onPressed: null,
+        ),
+        const SizedBox(width: 15),
+        const IconButton(
+          icon: Icon(Icons.notifications, color: Colors.black, size: 35),
+          onPressed: null,
+        ),
+      ],
+    );
+  }
+
+  Widget buildInputSearchBar(BuildContext context) {
+    return Row(
+      children: const [
+        Expanded(
+          child: TextField(
+            style: TextStyle(color: Colors.black, fontSize: 16),
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderSide: BorderSide.none),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderSide: BorderSide.none),
+                filled: true,
+                fillColor: Color(0xffeceef2),
+                hintText: 'Search',
+                prefixIcon: Icon(
+                  Icons.search,
+                  size: 24,
+                ),
+                isDense: true),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildAvailableProductSegments(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: CupertinoSlidingSegmentedControl<AvailableProductSegments>(
+            backgroundColor: Colors.transparent,
+            groupValue: _selectedSegment,
+            onValueChanged: (AvailableProductSegments? value) {
+              if (value != null) {
+                setState(() {
+                  _selectedSegment = value;
+                });
+              }
+            },
+            children: const <AvailableProductSegments, Widget>{
+              AvailableProductSegments.accounts: Text(
+                'Accounts',
+                style: TextStyle(color: Colors.black, fontSize: 16),
+              ),
+              AvailableProductSegments.cards: Text(
+                'Cards',
+                style: TextStyle(color: Colors.black, fontSize: 16),
+              ),
+              AvailableProductSegments.stocks: Text(
+                'Stocks',
+                style: TextStyle(color: Colors.black, fontSize: 16),
+              ),
+              AvailableProductSegments.crypto: Text(
+                'Crypto',
+                style: TextStyle(color: Colors.black, fontSize: 16),
+              ),
+              AvailableProductSegments.vaults: Text(
+                'Vaults',
+                style: TextStyle(color: Colors.black, fontSize: 16),
+              ),
+            },
+          ),
+        ),
+      ],
     );
   }
 }
