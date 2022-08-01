e6aa3a3f90a8985773e4036e40bd6558b34b2762
meruyert.bekzhigit <meruyert.bekzhigit@gmail.com>
2022-08-01 11:58:00 +0200

    Home screen header first row

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,12 +13,39 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color(0xffe5e8e8),
-        appBar: AppBar(
-          title: const Text('Revolut', style: TextStyle(color: Colors.black)),
-          shadowColor: Colors.transparent,
-          backgroundColor: Colors.transparent,
-        ),
-        body: Container());
+      backgroundColor: const Color(0xffe5e8e8),
+      appBar: AppBar(
+        title: const Text('Revolut', style: TextStyle(color: Colors.black)),
+        shadowColor: Colors.transparent,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Container(
+          margin:
+              const EdgeInsets.only(left: 16, top: 24, right: 16, bottom: 24),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  const Icon(Icons.add_a_photo, color: Colors.black, size: 30),
+                  Expanded(
+                    child: Container(),
+                  ),
+                  const Icon(Icons.bar_chart_rounded,
+                      color: Colors.black, size: 30),
+                  const SizedBox(width: 40),
+                  const Icon(Icons.star_rate_rounded,
+                      color: Colors.black, size: 30),
+                  const SizedBox(width: 40),
+                  const Icon(Icons.notifications,
+                      color: Colors.black, size: 30),
+                ],
+              ),
+              Row(
+                children: [],
+              ),
+              Row(),
+            ],
+          )),
+    );
   }
 }
